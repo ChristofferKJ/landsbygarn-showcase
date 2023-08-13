befe1db5f8345e4eb16d40a5622c9a19f9db2516
Christoffer Kjellerup Jakobsen <ckjn@kamstrup.com>
2023-08-13 19:45:37 +0200

    ny

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { Route, Routes } from "react-router";
 import { BrowserRouter } from "react-router-dom";
 import Contact from "./components/Contact";
 import About from "./components/About";
+import LandingPage from "./components/LandingPage";
 
 
 function App() {
@@ -17,9 +18,9 @@ function App() {
       <Logo />
       <TopNavBar className="navbar" />
       
-      <div className="routes-wrapper">
+      <div className="routes-wrapper px-40">
             <Routes>
-              <Route exact path="/" />
+              <Route exact path="/" element={<LandingPage/>}/>
               <Route exact path="/products" element={<ProductsGrid />} />
               <Route exact path="/about" element={<About />} />
               <Route exact path="/contact" element={<ProductsGrid />} />
---
+++ src/components/LandingPage.js
@@ -0,0 +1,27 @@
+import React, { useRef, useEffect } from 'react';
+
+const LandingPage = () => {
+
+
+    return (
+        <div class="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-8">
+        <div class="h-32 rounded-lg bg-gray-100">
+
+            <img src='https://landsbygarn.dk/images/landingpage/drops-kid-silk.jpg'/>
+
+
+        </div>
+        <div class="h-32 rounded-lg bg-gray-100">
+
+
+
+
+
+        </div>
+      </div>
+
+    )
+};
+
+export default LandingPage;
+
