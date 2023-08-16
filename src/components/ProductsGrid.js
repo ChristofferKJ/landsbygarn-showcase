c328b06b16afbb21edb4fa49fb12f018c061e66b
Christoffer Kjellerup Jakobsen <ckjn@kamstrup.com>
2023-08-16 20:01:38 +0200

    mer

--- src/components/ProductsGrid.js
+++ src/components/ProductsGrid.js
@@ -17,9 +17,9 @@ const ProductsGrid = () => {
         console.debug(process.env.NODE_ENV);
 
         if (process.env.NODE_ENV === "production") {
-           response = await fetch('api/fetchalltypes.php');
+          response = await fetch('api/fetchalltypes.php');
         } else if (process.env.NODE_ENV === "development") {
-           response = await fetch("fetchalltypes.json");
+          response = await fetch("fetchalltypes.json");
         }
 
         const data = await response.json();
@@ -54,7 +54,10 @@ const ProductsGrid = () => {
         {products.map((product) => (
           <div
             key={product?.product_name}
-            onClick={() => openPopup(product)}
+            onClick={() => {
+              setSelectedProduct(product);
+              window.my_modal_2.showModal();
+            }}
             className="cursor-pointer">
             <img
               className="h-auto max-w-full rounded-lg content-center"
@@ -65,32 +68,18 @@ const ProductsGrid = () => {
         ))}
       </div>
 
-      {isPopupOpen && (
-        <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50">
-          <div className="bg-white flex gap-10 p-4 rounded-lg shadow-lg w-1/2 h-1/2">
-            <img
-              className="max-w-full h-full rounded-lg"
-              src={selectedProduct?.image_url}
-              alt={selectedProduct?.product_name} />
+      <dialog id="my_modal_2" className="modal">
+        <form method="dialog" className="modal-box">
+          <h3 className="font-bold text-lg">{selectedProduct?.product_name}</h3>
+          <img src={selectedProduct?.image_url} />
+          <p>{selectedProduct?.title_text}</p>
 
-            <div className="flex flex-col">
-              <div>
-                <h3 className="font-bold text-xl mb-4">{selectedProduct?.product_name}</h3>
-                <p className="text-lg font-semibold">{selectedProduct?.title_text}</p>
-              </div>
-
-              <div className="flex flex-col gap-4">
-                <p>{selectedProduct?.made_of}</p>
-                {/* <p className="line-clamp-4">{selectedProduct?.description}</p> */}
-                <button onClick={closePopup} type="button" className="text-white bg-gradient-to-br from-red-600 to-red-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Gå tilBACH</button>
-                <a class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" href={"/products/" + selectedProduct?.product_name}> gå til produkt</a>
-
-                {/* <button onClick={navigate("/products/" + selectedProduct?.product_name)} type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Gå til produkt</button> */}
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
+          <button className="btn">Gå til produkt</button>
+        </form>
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
     </div>
   );
 };
